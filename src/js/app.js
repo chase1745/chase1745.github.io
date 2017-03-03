3a4718eba5f5587633de45540b1fb906689f9572
Chase <chasemcd1745@tamu.edu>
2017-03-03 16:45:45 -0600

    Changed Colors.

--- src/js/app.js
+++ src/js/app.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }
       },
       "color": {
-        "value": "#ffffff"
+        "value": "#D70C0C"
       },
       "shape": {
         "type": "polygon",
@@ -77,8 +77,8 @@ document.addEventListener("DOMContentLoaded", function () {
       "detect_on": "window",
       "events": {
         "onhover": {
-          "enable": false,
-          "mode": "grab"
+          "enable": true,
+          "mode": "repulse"
         },
         "onclick": {
           "enable": true,
